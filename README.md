cbfc71ac691a2af01239329d72310ad0a75d1419
Ryan Woo <devrwoo@gmail.com>
2019-06-21 13:38:59 -0700

    Examples Added

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 1. Learn Typescript:
 
 - What is Typescript and when typescript kicks in.. (fetchjson folder for quick reference)
+
+2. Examples of Typescripts: (features folder)
---
+++ features/index.ts
@@ -0,0 +1,13 @@
+const today = new Date();
+
+today.getMonth();
+// today.asdklfj gets an error b/c its not a date.
+
+const person = {
+  age: 20
+};
+// person.lksadjf gets an error b/c it doesn't have that property
+
+class Color {}
+
+const red = new Color();
